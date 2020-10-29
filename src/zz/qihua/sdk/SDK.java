1884a1b6219c3860552fadb0d59057ac6d8464e1
fufds <352568145@qq.com>
2020-10-29 22:05:22 +0800

    构建项目框架

---
+++ src/zz/qihua/sdk/SDK.java
@@ -0,0 +1,10 @@
+package zz.qihua.sdk;
+
+public class SDK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ttt/src/ttt/foo/Foo.java
+++
@@ -1,5 +0,0 @@
-package ttt.foo;
-
-public class Foo {
-
-}
